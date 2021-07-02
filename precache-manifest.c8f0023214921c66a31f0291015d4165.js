918089b4d79fd093f5f87d6f3b990e700d3fe3d1
Nate Denys <nate_denys@yahoo.com>
2021-07-02 14:53:10 -0700

    deployed to github

---
+++ precache-manifest.c8f0023214921c66a31f0291015d4165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a548115187e390e43d41",
+    "url": "/ticket_tracker/css/app.629f20f9.css"
+  },
+  {
+    "revision": "e7147278684e2e1e2e068ff38df664f7",
+    "url": "/ticket_tracker/index.html"
+  },
+  {
+    "revision": "a548115187e390e43d41",
+    "url": "/ticket_tracker/js/app.026b0915.js"
+  },
+  {
+    "revision": "1fdc35af6fb32b249ac2",
+    "url": "/ticket_tracker/js/chunk-vendors.0e6875e6.js"
+  },
+  {
+    "revision": "c8537f4fad11f55b57c96f65f37d8934",
+    "url": "/ticket_tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ticket_tracker/robots.txt"
+  }
+]);
